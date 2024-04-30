526495c7b783ae64c205b68126fabcb75af7c246
JaredKirsch20 <159041808+JaredKirsch20@users.noreply.github.com>
2024-04-30 17:22:29 -0400

    oiiujh

---
+++ PolynomialTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PolynomialTest {
+
+    @Test
+    public void Polynomial(){
+        assertEquals(0,0);
+    }
+
+
+    @Test
+    public void evaluate(int x) {
+        assertEquals(x, 0);
+    }
+    // @Test
+    // public void sortPoly() {
+    //     assertTrue(true);
+    // }
+    // @Test
+    // public void swap() {
+    //     assertTrue(true);
+    // }
+    // @Test
+    // public String toString(String subString) {
+    //     assertTrue(true);
+    //     return subString;
+    // }
+    // @Test
+    // public void getOrder() {
+    //     assertTrue(true);
+    // }
+    // @Test
+    // public void addTerm() {
+    //     assertTrue(true);
+    // }
+}
